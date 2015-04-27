1b8b6bd23ef3c1fe877c61fffebef159335ce1e0
imbolc <imbolc@imbolc.name>
2015-04-28 04:59:07 +0700

    simple-python-package template

---
+++ simple-python-package/my_package.py
@@ -0,0 +1,19 @@
+'''
+Module
+======
+Module description
+
+Install
+-------
+    pip install module
+
+Usage
+-----
+    >>> 1 == True
+    True
+'''
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
